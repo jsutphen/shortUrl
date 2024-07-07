9fad6648db0d6deb155010e70f316c1c710a097d
jsutphen <jonathan@sutphen.de>
2024-07-07 21:55:12 +0200

    add post request logic to router and fix template form

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,32 @@ const Url = require('../models/url');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { shortUrl: '1234' });
+  res.render('index');
 });
+
+function makeId() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  const LENGTH = 4;
+  for (i = 0; i < LENGTH; i++) {
+    const nextLetter = characters.charAt(Math.floor(Math.random() * characters.length));
+    result += nextLetter;
+  }
+  return result;
+}
+
+router.post('/', asyncHandler(async (req, res, next) => {
+  const originalUrl = req.body.originalUrl;
+  let shortUrl;
+  while(true) {
+    shortUrl = makeId();
+    const alreadyExistsTest = await Url.findOne({ shortUrl: shortUrl });
+    if (alreadyExistsTest === null) break;
+  }
+  const url = new Url({ originalUrl: originalUrl, shortUrl: shortUrl });
+  await url.save();
+  res.render('index', { shortUrl: shortUrl });
+}));
 
 router.get('/:shortUrl', asyncHandler(async (req, res, next) => {
   const url = await Url.findOne({ shortUrl: req.params.shortUrl });
