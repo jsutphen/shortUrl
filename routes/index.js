658ae4024e42818f2a75673883f0450ec76f3f23
jsutphen <jonathan@sutphen.de>
2024-07-07 21:13:21 +0200

    add redirect logic

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const asyncHandler = require('express-async-handler');
+const Url = require('../models/url');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { shortUrl: '1234' });
 });
 
-module.exports = router;
+router.get('/:shortUrl', asyncHandler(async (req, res, next) => {
+  const url = await Url.findOne({ shortUrl: req.params.shortUrl });
+  if (url === null) {
+    res.send('page not found');
+  } else {
+    const originalUrl = url.originalUrl;
+    res.status(301).redirect('https://' + originalUrl);
+  }
+}));
+
+module.exports = router; 
