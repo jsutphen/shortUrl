9599763c3eeb8258014156cb6435322235df5f72
jsutphen <jonathan@sutphen.de>
2024-07-08 22:58:09 +0200

    add expiry (2 weeks) to model

--- models/url.js
+++ models/url.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 
 const urlSchema = new Schema({
     originalUrl: { type: String, required: true },
-    shortUrl: { type: String, required: true }
-})
+    shortUrl: { type: String, required: true },
+    timeCreated: { type: Date, required: true, default: Date.now, expires: 1209600 /*two weeks*/ }
+});
 
 module.exports = mongoose.model('url', urlSchema);
